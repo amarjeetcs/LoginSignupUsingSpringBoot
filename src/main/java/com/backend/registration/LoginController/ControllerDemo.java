db398bc2f025b7b9e7f81923eb633b0d2c979221
amarjeet kumar singh <amarjeetcs79@gmail.com>
2025-07-18 04:39:57 -0700

    Second commit

--- src/main/java/com/backend/registration/LoginController/ControllerDemo.java
+++ src/main/java/com/backend/registration/LoginController/ControllerDemo.java
@@ -1,7 +1,13 @@
 package com.backend.registration.LoginController;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ControllerDemo {
+
+    @GetMapping("/Hello")
+    public String getName(){
+        return "Amarjeet Singh";
+    }
 }
